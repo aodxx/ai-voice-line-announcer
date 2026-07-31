8391c2940da8cedfb395d7f2b48d05fc931685af
aodxx <aodaod3826@gmail.com>
2026-08-01 04:06:19 +0800

    เปิดใช้ authenticated fetch

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import AuthGate from './components/AuthGate.tsx';
 import { initializeFirebaseAnalytics } from './lib/firebase';
+import { installAuthenticatedFetch } from './lib/authenticated-fetch';
 import './index.css';
 
+installAuthenticatedFetch();
 void initializeFirebaseAnalytics();
 
 createRoot(document.getElementById('root')!).render(
